eef061ec837a0e3d878b34127e68e1e16a4bcaf0
kitchu-sunil <76254824+kitchu-sunil@users.noreply.github.com>
2021-06-30 20:51:59 +0530

    serviceWorker.register error corrected

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { HashRouter as Router } from "react-router-dom";
-import * as serviceWorker from "./serviceWorkerRegister";
+import * as serviceWorker from "./serviceWorkerRegistration";
 ReactDOM.render(
   <React.StrictMode>
     <Router>
